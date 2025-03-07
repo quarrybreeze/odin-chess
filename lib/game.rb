5fabf6f9853fc9e835e78c044897234bfdc39df4
Terrence Clayton <184171442+quarrybreeze@users.noreply.github.com>
2025-03-07 11:03:27 -0800

    Initialize chess game structure with necessary files and dependencies

---
+++ lib/game.rb
@@ -0,0 +1,3 @@
+require_relative 'board'
+require_relative 'computer'
+require_relative 'player'
---
+++ spec/piece_spec.rb
@@ -0,0 +1,7 @@
+require_relative '../lib/pieces/piece'
+require_relative '../lib/pieces/bishop'
+require_relative '../lib/pieces/king'
+require_relative '../lib/pieces/knight'
+require_relative '../lib/pieces/pawn'
+require_relative '../lib/pieces/queen'
+require_relative '../lib/pieces/rook'
