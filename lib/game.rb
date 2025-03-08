17a65dc5711717a4b2eabcb8a016afa6f476c8b9
Terrence Clayton <184171442+quarrybreeze@users.noreply.github.com>
2025-03-08 00:14:07 -0800

    added logic to stop pieces from jumping over eachother.
    added win condition if they take king.
    
    todo: add logic to check for checks and checkmates.
    add logic to prevent king from moving into a check position.

--- lib/game.rb
+++ lib/game.rb
@@ -9,10 +9,22 @@ class Game
     @players = []
     @gameover = false
     self.play_game
+    # p self.jump?(@board.pick_tile(2,0),[0,2])
   end
 
   def display
     @board.display
+  end
+
+  def king?
+    array_of_pieces = @board.tiles.flatten(2)
+    alive_kings = array_of_pieces.select { |tiles| tiles.name == "king"}
+
+    if alive_kings.length < 2
+      puts "King down."
+      puts "Winner is #{alive_kings.first.color}"
+      @gameover = true
+    end
   end
 
   def make_move (name,coordinates)
@@ -55,17 +67,115 @@ class Game
         p piece.position
       end
       specific_piece = gets.chomp.split(",").map(&:to_i)
-      @board.move(specific_piece,coordinates)
+      specific_piece_x = specific_piece.first
+      specific_piece_y = specific_piece.last
+      if jump?(@board.pick_tile(specific_piece_x,specific_piece_y),coordinates)
+        puts "The #{name} cannot jump over pieces. Please choose a valid move"
+        self.player_turn
+      else
+        @board.move(specific_piece,coordinates)
+      end
     elsif matching_pieces.length == 0 #if invalid move, loop
-      p "error, not a valid move, please try again"
+      p "The #{name}, cannot move there. Please choose a valid move"
       self.player_turn
     else
       current_tile = matching_pieces.first.position
-      @board.move(current_tile,coordinates)
+      piece = matching_pieces.first
+      if jump?(piece,coordinates)
+        puts "The #{name} cannot jump over pieces. Please choose a valid move"
+        self.player_turn
+      else
+        @board.move(current_tile,coordinates)
+      end
     end
   end
 
   private
+
+  def jump?(object,target)
+    jump = false
+    tile_queue = []
+
+    start_x = object.position.first
+    start_y = object.position.last
+    target_x = target.first
+    target_y = target.last
+
+    if (start_y == target_y && #right
+      target_x > start_x)
+      # p "right"
+      for x in start_x..target_x
+        tile_queue << [x, start_y]
+      end
+    elsif (start_y == target_y && #left
+          target_x < start_x)
+          # p "left"
+      for x in target_x..start_x
+        tile_queue << [x, start_y]
+      end
+    elsif (start_x == target_x && #up
+          target_y > start_y)
+          # p "up"
+      for y in start_y..target_y
+        tile_queue << [start_x, y]
+      end
+    elsif (start_x == target_x && #down
+          target_y < start_y)
+          # p "down"
+      for y in target_y..start_y
+        tile_queue << [start_x, y]
+      end
+    elsif (start_y < target_y && #upright
+        start_x < target_x)
+        diag_y = start_y
+      # p "upright"
+      for x in start_x..target_x
+        tile_queue << [x,diag_y]
+        diag_y += 1
+      end
+    elsif (start_y < target_y && #upleft
+      start_x > target_x)
+      # p "upleft"
+      diag_x = start_x
+      for y in start_y..target_y
+        tile_queue << [diag_x,y]
+        diag_x -= 1
+      end
+    elsif (start_y > target_y && #downright
+      start_x < target_x)
+      diag_y = start_y
+      # p "downright"
+      for x in start_x..target_x
+        tile_queue << [x,diag_y]
+        diag_y -= 1
+      end
+    elsif (start_y > target_y && #downleft
+      start_x > target_x)
+      diag_x = target_x
+      # p "downleft"
+      for y in target_y..start_y
+        tile_queue << [diag_x,y]
+        diag_x += 1
+      end
+    end
+
+    tile_queue.pop
+    tile_queue.shift
+
+    occupied_tiles = tile_queue.select { |element| 
+    @board.pick_tile(element.first,element.last).symbol != " " 
+    }
+
+    if occupied_tiles.length > 0
+      jump = true
+    end
+
+    if object.name == "knight"
+      jump = false
+    end
+
+    return jump
+  end
 
   def player_turn
     current_player = @players[0].color
@@ -91,6 +201,7 @@ class Game
     self.white_first
     while @gameover == false
       self.player_turn
+      self.king?
       self.switch_player
     end
 
