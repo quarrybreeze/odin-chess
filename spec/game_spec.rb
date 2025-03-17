42631653013f740d70f6391d249de8230c44e781
Terrence Clayton <184171442+quarrybreeze@users.noreply.github.com>
2025-03-16 21:19:51 -0700

    Remove commented-out code and improve game_spec readability

--- spec/game_spec.rb
+++ spec/game_spec.rb
@@ -27,4 +27,73 @@ describe Game do
       end
     end
   end
+
+  # let(:game) do
+  #   # Stub all gets calls before creating the game instance
+  #   allow_any_instance_of(Game).to receive(:gets).and_return("new")
+  #   allow_any_instance_of(Player).to receive(:gets).and_return("white")
+  #   Game.new
+
+  #   # Stub player_turn to prevent infinite recursion
+  #   allow(game).to receive(:player_turn)
+
+  #   game
+  # end
+
+  # before do
+  #   # Stub file operations
+  #   allow(File).to receive(:exist?).with("savedgame.dat").and_return(false)
+  # end
+
+  # describe '#king?' do
+  #   context 'when both kings are alive' do
+  #     it 'returns false for gameover' do
+  #       expect(game.instance_variable_get(:@gameover)).to be false
+  #     end
+  #   end
+  # end
+
+  # describe '#make_move' do
+  #   context 'when moving a pawn' do
+  #     it 'prevents moving to occupied spaces by friendly pieces' do
+  #       allow(game).to receive(:player_turn)
+  #       game.make_move('pawn', [1, 1])
+  #       expect(game).to have_received(:player_turn)
+  #     end
+  #   end
+  # end
+
+  # describe '#checkmate?' do
+  #   context 'when king is not in checkmate' do
+  #     it 'returns false' do
+  #       expect(game.send(:checkmate?)).to be false
+  #     end
+  #   end
+  # end
+
+  # describe '#check?' do
+  #   context 'when king is not in check' do
+  #     it 'returns false' do
+  #       expect(game.send(:check?)).to be false
+  #     end
+  #   end
+  # end
+
+  # describe '#switch_player' do
+  #   it 'rotates the players array' do
+  #     original_first_player = game.instance_variable_get(:@players).first
+  #     game.send(:switch_player)
+  #     new_first_player = game.instance_variable_get(:@players).first
+  #     expect(new_first_player).not_to eq(original_first_player)
+  #   end
+  # end
+
+  # describe '#white_first' do
+  #   it 'ensures white player goes first' do
+  #     game.send(:white_first)
+  #     first_player = game.instance_variable_get(:@players).first
+  #     expect(first_player.color).to eq('white')
+  #   end
+  # end
+
 end
